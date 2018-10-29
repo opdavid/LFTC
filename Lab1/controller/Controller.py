d21a32b2031203462dae46e7e62724bc2e023847
DAVID-PC\David <oprutadavid@gmail.com>
2018-10-29 07:52:51 +0200

    changes

--- Lab1/controller/Controller.py
+++ Lab1/controller/Controller.py
@@ -1,21 +1,35 @@
 import re
 from domain.SimbolTable import SimbolTable
+from domain.CodingTable import CodingTable
 
 
 class Controller:
 
     def __init__(self):
-        ST = SimbolTable("zor")
-        for i in ["ana", "gama", "cri", "fic"]:
-            ST.insert_val(i)
-
-        print(ST.display(ST))
+        self.ST = SimbolTable(-1)
+        self.CT = CodingTable()
+        self.numberRegex = re.compile('[0-9]')
+        self.identRegex = re.compile('\w')
+        self.reservedWords = ["begin", "end", "program", "and", "not", "or", "for", "else", "if", "then", "do", "read",
+                              "while", "write", "integer", "var", "array"]
 
     def readFromFile(self, filename):
         with open(filename, "r") as f:
             for line in f:
-                l = re.split('(\W)', line)
-                print(l)
-                # for token in l:
-                #     if token != '\n' or token != ' ' or token != '':
-                #         print(token)
+                token_list = re.split('(\W)', line)
+                for token in token_list:
+                    if token != '\n' and token != ' ' and token != '':
+                        if self.numberRegex.match(token):
+                            self.ST.insert_val(self.CT.get_value_for_key("constant"))
+                        elif token in self.reservedWords:
+                            self.ST.insert_val(self.CT.get_value_for_key(token))
+                        elif self.identRegex.match(token):
+                            self.ST.insert_val(self.CT.get_value_for_key("identifier"))
+                        else:
+                            try:
+                                code = self.CT.get_value_for_key(token)
+                                self.ST.insert_val(code)
+                            except Exception:
+                                print("Invalid token: " + token)
+                        # print(token)
+        print(self.ST.display(self.ST))
---
+++ Lab1/domain/CodingTable.py
@@ -0,0 +1,52 @@
+class CodingTable:
+
+    def __init__(self):
+        self.dict = {
+            "identifier": 0,
+            "constant": 1,
+            "program": 2,
+            "array": 3,
+            "of": 4,
+            "var": 5,
+            "integer": 6,
+            "real": 7,
+            "boolean": 8,
+            "begin": 9,
+            "end": 10,
+            "read": 11,
+            "write": 12,
+            "for": 13,
+            "to": 14,
+            "do": 15,
+            "if": 16,
+            "then": 17,
+            "else": 18,
+            "and": 19,
+            "or": 20,
+            "not": 21,
+            ":": 22,
+            ";": 23,
+            ",": 24,
+            ".": 25,
+            "+": 26,
+            "*": 27,
+            "(": 28,
+            ")": 29,
+            "[": 30,
+            "]": 31,
+            "-": 32,
+            "<": 33,
+            ">": 34,
+            "=": 35,
+            "{": 36,
+            "}": 37,
+            "declare": 38,
+            "while": 39,
+        }
+        # print(self.dict)
+
+    def get_value_for_key(self, key):
+        return self.dict[key]
+
+    def get_dict(self):
+        return dict
